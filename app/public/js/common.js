3d2b1ccd6b59adfa196fee91ee4aff3937fc46e1
胡元港 <1730790894@qq.com>
2017-03-01 17:35:17 +0800

    封装日期格式化函数

--- app/public/js/common.js
+++ app/public/js/common.js
@@ -54,5 +54,42 @@ module.exports = {
 			}
 			xhr.send(sendData);
 		}
+	},
+	date:{
+		format:function(dateStr, format){
+			var date = this.toDate(dateStr);
+			if(!(date instanceof Date))
+				return '格式化串错误';
+			var RegObj = {
+				"M+": date.getMonth()+1,
+				"d+": date.getDate(),
+				"h+": date.getHours()%12 == 0 ? 12 :  date.getHours(),
+				"H+": date.getHours(),
+				"m+": date.getMinutes(),
+				"s+": date.getSeconds()
+			}
+			if(/(y+)/.test(format)){
+				format = format.replace(RegExp.$1,(date.getFullYear() + '').substr(4-RegExp.$1.length));
+			}
+			for(var reg in RegObj){
+				if(RegObj.hasOwnProperty(reg)){
+					if(new RegExp('('+ reg + ')').test(format)){
+						format = format.replace(RegExp.$1,(RegExp.$1.length==1 ? RegObj[reg] : ('00' + RegObj[reg]).substr((''+RegObj[reg]).length)));
+					}
+				}
+			}
+			return format;
+		},
+		toDate:function(dateStr){
+			if(typeof dateStr === 'string'){
+				return new Date(parseInt(dataStr))
+			}
+			else if(typeof dateStr === 'number'){
+				return new Date(dateStr)
+			}
+			else{
+				return dateStr;
+			}
+		}
 	}
 }
